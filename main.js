7296b8410bd2e9dad57d816372eacc3a5a39bf03
anshul <anshultamrakar487@gmail.com>
2024-02-26 16:20:05 +0530

    more js question

--- main.js
+++ main.js
@@ -583,6 +583,113 @@
 //    }
 //    return sum/arr.length
 // }
+//  console.log(calculateAverage([1, 2, 3, 4, 5]))
 
-// console.log(calculateAverage([1, 2, 3, 4, 5]))
 
+
+// write an ES6 function that takes an array of numbers and converts even numbers to odd numbers by adding 1 to that number
+
+// const convertEvenToOdd = (arr) => {
+//   let myArr = [];
+//   for(let i = 0 ; i < arr.length ; i++){
+//     if(arr[i] % 2 === 0){
+//       myArr.push(arr[i] + 1)
+//     }else{
+//       myArr.push(arr[i])
+//     }
+//   }
+
+//   return myArr
+// }
+
+
+// console.log(convertEvenToOdd([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+
+// Write an ES6 function that takes an array of words and returns an array with all its elements whose length is greater than 5.
+
+//  const filterWords = (arr) => {
+//    let myArr = [];
+//    for(let i = 0 ; i < arr.length ; i++){
+//      if(arr[i].length > 5){
+//       myArr.push(arr[i])
+//      }
+//    }
+//    return myArr
+//  }
+
+//  console.log(filterWords( ['eat', 'sleep', 'code', 'repeat', 'neog', 'community']))
+
+
+
+// Write an ES6 function that takes an array of strings and returns a new array with each string capitalized.
+
+    // const capitalizeWords = (arr) => {
+    //  let myArr = [];
+    //  for(let i = 0 ; i < arr.length ; i++){
+    //   myArr.push(arr[i].toUpperCase())
+    //  }
+    //   return myArr;
+    // }
+
+    // console.log(capitalizeWords(['eat', 'sleep', 'code', 'repeat']))
+
+// Write an ES6 function that takes an array of objects and a property name and returns a new array with only the values of that property. 
+
+  //  Avoid using in-built methods.
+
+  // const getValues = (arr ) => {
+  //    let newArray = [];
+  //    for(let i = 0 ; i < arr.length ; i++){
+  //      newArray.push(arr[i].name)
+  //    }
+  //    return newArray;
+  // }
+
+  // console.log(getValues([{ name: 'John', age: 21 }, { name: 'Mary', age: 22 }, { name: 'Peter', age: 23 }]))
+
+
+  // Write an ES6 function that takes the users' details and returns the data with team ID. Avoid using in-built methods.
+  
+
+  // {firstName: 'John', lastName: 'Dee', teamId: 667543}
+
+
+  // const podAndTeamAllocation = (arr) => {
+  //       return {...arr , teamId : "68886"}
+  // }
+
+  // console.log(podAndTeamAllocation({ firstName: 'John', lastName: 'Dee' }))
+
+
+  // Write an ES6 function which checks if a student already has a team. If team is not given then add them to team “A” and return the object else do nothing and return the same object.
+  //  Avoid using in-built methods.
+
+
+
+  // Write an ES6 function that takes an array of objects and returns the sum of all ages
+    
+
+  // const sumOfAges = (arr) => {
+  //   let sum = 0
+  //   for(let i = 0 ; i < arr.length ; i++){
+  //     sum = arr[i].age + sum
+  //   }
+  //   return sum
+  // }
+
+  // console.log(sumOfAges([{name: 'Jay', age: 60},  {name: 'Gloria',    age: 36 },  { name: 'Manny', age: 16 },  { name: 'Joe', age: 9,  }]))
+
+
+  // Destructure the following code to get the desired outputs. Avoid using in-built methods
+
+  const book = { title: 'JavaScript: The Definitive Guide',  authors: [{ name: 'David Flanagan', age: 49 }, { name: 'Yukihiro Matsumoto', age: 57 }],  publisher: { name: "O'Reilly Media", location: 'CA' }}
+
+  const {title , authors : [author1 , author2] } = book
+  const {name} = author1
+  
+
+  // Your ES6 code here console.log(title) 
+  // JavaScript: The Definitive Guide console.log(author1) 
+  // David Flanaganconsole.log(author2) 
+  // Yukihiro Matsumoto console.log(publisherName) 
