07792b7e98037efc3ab6485c799e7b4600128553
anshul <anshultamrakar487@gmail.com>
2024-02-26 16:20:52 +0530

    fix : bugs

--- main.js
+++ main.js
@@ -683,10 +683,6 @@
 
   // Destructure the following code to get the desired outputs. Avoid using in-built methods
 
-  const book = { title: 'JavaScript: The Definitive Guide',  authors: [{ name: 'David Flanagan', age: 49 }, { name: 'Yukihiro Matsumoto', age: 57 }],  publisher: { name: "O'Reilly Media", location: 'CA' }}
-
-  const {title , authors : [author1 , author2] } = book
-  const {name} = author1
   
 
   // Your ES6 code here console.log(title) 
