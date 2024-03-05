c477ba16188b9805cb1aeb965da5abdeaf980fe8
anshul <anshultamrakar487@gmail.com>
2024-03-05 18:28:51 +0530

    started set 3 FP

--- main.js
+++ main.js
@@ -1050,3 +1050,145 @@
 
 // const bestOldMovies = getBestOldBollywoodMovies(bollywoodMovies)
 // console.log(bestOldMovies)
+
+
+
+
+
+
+// FP Set 3 ----------------------------------------------------------------------------------------------------
+
+// Write an ES6 function that takes an array of objects representing cars with properties make, model, and year. 
+// Return the first car object that is a Toyota and the year is after 2010.
+
+// const cars = [{
+//   make: 'Toyota',
+//   model: 'Corolla',
+//   year: 2010
+// }, {
+//   make: 'Honda',
+//   model: 'Civic',
+//   year: 2012
+// }, {
+//   make: 'Toyota',
+//   model: 'Camry',
+//   year: 2015
+// }, {
+//   make: 'Ford',
+//   model: 'Mustang',
+//   year: 2018
+// }]
+
+// const findToyotaCar = (arr) => {
+//  return arr.find(item => item.make === "Toyota" && item.year > 2010)
+// }
+
+// const toyotaCar = findToyotaCar(cars)
+// console.log(toyotaCar)
+
+
+// Write an ES6 function that takes an array of objects containing car information (make, model, year) 
+// and returns an array with only the cars that were made after the year 2012.
+// const cars = [  { make: 'Toyota', model: 'Corolla', year: 2010 },  { make: 'Honda', model: 'Civic', year: 2012 },  { make: 'Toyota', model: 'Camry', year: 2015 },  { make: 'Ford', model: 'Mustang', year: 2018 }]
+
+//  const getCarDetails = (arr) => {
+//   return arr.filter(item => item.year > 2012) 
+//  }
+
+//  console.log(getCarDetails(cars))
+
+
+// Write an ES6 function that takes an array of objects representing products with properties name, price, and category. 
+// Return the first product object that is in the category "electronics".
+
+
+// const products = [{ name: 'Toothbrush', price: 29, category: 'health' },  { name: 'Coffee Maker', price: 99, category: 'home' },  { name: 'iPad', price: 799, category: 'electronics' },  { name: 'Smartwatch', price: 199, category: 'electronics' }]
+
+// const findElectronicsProduct  = (arr) => {
+//   return arr.find(item => item.category === "electronics")
+// }
+
+// console.log(findElectronicsProduct(products))
+
+
+
+// Write an ES6 function that takes an array of objects representing products with properties name, price and category. 
+// Return all the products object that are in the category "electronics".
+
+// const products = [  { name: 'Toothbrush', price: 29, category: 'health' },  { name: 'Coffee Maker', price: 99, category: 'home' },  { name: 'iPad', price: 799, category: 'electronics' },  { name: 'Smartwatch', price: 199, category: 'electronics' }]
+
+// const getAllElectronicProducts = (arr) => {
+//   return arr.filter(item => item.category === "electronics") 
+// }
+
+// console.log(getAllElectronicProducts(products))
+
+
+// Write an ES6 function that takes an array of objects containing student information (name, age, grade) 
+// and returns an array with only the students who have a grade above a certain value.
+
+// const students = [ { name: 'Alice', age: 16, grade: 90 },  { name: 'Bob', age: 17, grade: 80 },  { name: 'Charlie', age: 15, grade: 95 },  { name: 'David', age: 16, grade: 85 }]
+
+// const highGradeStudents = (arr , val) => {
+//   return arr.filter(item => item.grade > val)
+// }
+
+// console.log(highGradeStudents(students , 85))
+
+
+
+// Write an ES6 function that takes an array of objects representing books with properties title, author and pageCount. 
+// Return the first book object that has more than 500 pages.
+
+// const books = [{ title: 'The Lord of the Rings', author: 'J.R.R. Tolkien',pageCount: 1178,  },  { title: 'To Kill a Mockingbird', author: 'Harper Lee', pageCount: 281 },  { title: "The Hitchhiker's Guide to the Galaxy", author: 'Douglas Adams',  pageCount: 193,  },  { title: 'The Name of the Wind',    author: 'Patrick Rothfuss',    pageCount: 662 }]
+
+
+
+// const findBookWithMoreThan500Pages = (arr) => {
+//    return arr.find(item => item.pageCount > 500)
+// }
+
+
+// console.log(findBookWithMoreThan500Pages(books))
+
+
+// Write an ES6 function that takes an array of objects containing customer information (name, age, gender) 
+// and returns an array with only the customers who are of gender Male.
+
+// const customers = [  { name: 'John', age: 25, gender: 'Male' },  { name: 'Jane', age: 30, gender: 'Female' },  { name: 'Bob', age: 40, gender: 'Male' },  { name: 'Alice', age: 35, gender: 'Female' },]
+
+// const getMaleCustomers = (arr) => {
+//   return arr.filter(item => item.gender === "Male")
+// }
+
+// console.log(getMaleCustomers(customers))
+
+
+
+// Write an ES6 function that takes an array of objects containing game information (title, developer, genre) 
+// and returns an array with all the games of a certain genre.
+
+
+// const games = [{ title: 'The Witcher 3: Wild Hunt', developer: 'CD Projekt Red',    genre: 'RPG',  },  {    title: 'Grand Theft Auto V', developer: 'Rockstar North',    genre: 'Action',  },  { title: 'Portal 2', developer: 'Valve Corporation', genre: 'Puzzle' },  {    title: 'The Legend of Zelda: Breath of the Wild', developer: 'Nintendo',    genre: 'Adventure'}]
+
+// const filterByGenre = (arr , valGenre ) => {
+//   return arr.filter(item => item.genre === valGenre)
+// }
+
+// console.log(filterByGenre(games , "RPG"))
+
+// Write an ES6 function that takes an array of objects containing car information (make, model, year) 
+// and returns an array with only the car model that were made after the year 2012.
+
+
+// const cars = [{ make: 'Toyota', model: 'Corolla', year: 2010 },{ make: 'Honda', model: 'Civic', year: 2012 },  
+// { make: 'Toyota', model: 'Camry', year: 2015 },  { make: 'Ford', model: 'Mustang', year: 2018 }]
+
+
+// const getCarModel = (arr) => {
+//   return  arr.filter(item => item.year > 2012).map(item => item.model)
+// }
+
+
+// console.log(getCarModel(cars))
+
